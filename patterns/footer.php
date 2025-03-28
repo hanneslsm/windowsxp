2c472c95717629fb38f3b08210053193ca6783d0
hanneslsm <HannesLeismann@gmail.com>
2025-03-28 23:01:20 +0100

    Create block theme save

---
+++ patterns/footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Title: footer
+ * Slug: windowsxp/footer
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"className":"is-style-xp-taskbar","style":{"dimensions":{"minHeight":"30px"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group is-style-xp-taskbar" style="min-height:30px"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://windowsxp.local/wp-content/uploads/2025/03/download.png" alt="" class=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:navigation {"ref":9,"icon":"menu","hasIcon":false,"className":"is-style-xp-taskbar\u002d\u002dnav","style":{"spacing":{"blockGap":"1px"}},"layout":{"type":"flex","justifyContent":"center"}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"is-style-xp-taskbar\u002d\u002dnotification-area","style":{"spacing":{"blockGap":"var:preset|spacing|10"},"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group is-style-xp-taskbar--notification-area"><!-- wp:image {"width":"16px","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="http://windowsxp.local/wp-content/uploads/2025/03/n37kfqi14nm9acabt6qfbfkiis-d26a5cea3de0e418b4be7e04a3564624.png" alt="" class="" style="width:16px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"width":"16px","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="http://windowsxp.local/wp-content/uploads/2025/03/Sound_Recorder_2001.webp" alt="" class="" style="width:16px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"width":"16px","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="http://windowsxp.local/wp-content/uploads/2025/03/pngimg.com-icq_PNG18.png" alt="" class="" style="width:16px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('12:12', 'windowsxp');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ patterns/index.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Title: index
+ * Slug: windowsxp/index
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"align":"wide","style":{"dimensions":{"minHeight":"100dvh"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between","justifyContent":"stretch"}} -->
+<div class="wp-block-group alignwide" style="min-height:100dvh"><!-- wp:group {"tagName":"main","metadata":{"name":"Main"},"style":{"spacing":{"margin":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"},"blockGap":"0"},"dimensions":{"minHeight":"100%"}},"layout":{"type":"constrained"}} -->
+<main class="wp-block-group" style="min-height:100%;margin-top:var(--wp--preset--spacing--60);margin-bottom:var(--wp--preset--spacing--60)"><!-- wp:heading {"className":"is-variation-title-bar"} -->
+<h2 class="wp-block-heading is-variation-title-bar"><?php esc_html_e('hello worldsad, asdkljsadlsajdlksadj ', 'windowsxp');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"className":"is-variation-notepad\u002d\u002dcontent","layout":{"type":"default"}} -->
+<div class="wp-block-group is-variation-notepad--content"><!-- wp:paragraph -->
+<p><?php esc_html_e('oh hi mark', 'windowsxp');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"is-variation-notepad\u002d\u002dcontent","layout":{"type":"default"}} -->
+<div class="wp-block-group is-variation-notepad--content"><!-- wp:paragraph -->
+<p><?php esc_html_e('hello world', 'windowsxp');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p><?php esc_html_e('oh hi div', 'windowsxp');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></main>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","tagName":"footer"} /--></div>
+<!-- /wp:group -->
